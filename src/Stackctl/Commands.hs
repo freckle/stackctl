82f43a76c92c443bb7ca37e37f9adcf1cb201527
patrick brisbin <pbrisbin@gmail.com>
2022-03-17 15:24:16 -0400

    Generalize sub-commands and help strings

--- src/Stackctl/Commands.hs
+++ src/Stackctl/Commands.hs
@@ -19,8 +19,8 @@ cat
   :: (HasLogFunc env, HasResourceMap env, HasAwsEnv env, HasOptions env)
   => Subcommand SpecCatOptions env
 cat = Subcommand
-  { name = "spec:cat"
-  , description = "Pretty-print a Stack specification"
+  { name = "cat"
+  , description = "Pretty-print specifications"
   , parse = parseSpecCatOptions
   , run = runSpecCat
   }
@@ -29,8 +29,8 @@ capture
   :: (HasLogFunc env, HasResourceMap env, HasAwsEnv env)
   => Subcommand SpecCaptureOptions env
 capture = Subcommand
-  { name = "spec:capture"
-  , description = "Capture an App's Stack specification"
+  { name = "capture"
+  , description = "Capture deployed Stacks as specifications"
   , parse = parseSpecCaptureOptions
   , run = runSpecCapture
   }
@@ -39,8 +39,8 @@ changes
   :: (HasLogFunc env, HasResourceMap env, HasAwsEnv env, HasOptions env)
   => Subcommand SpecChangesOptions env
 changes = Subcommand
-  { name = "spec:changes"
-  , description = "Create and review changes from an App's Stack specification"
+  { name = "changes"
+  , description = "Review changes between specification and deployed state"
   , parse = parseSpecChangesOptions
   , run = runSpecChanges
   }
@@ -49,8 +49,8 @@ deploy
   :: (HasLogFunc env, HasResourceMap env, HasAwsEnv env)
   => Subcommand SpecDeployOptions env
 deploy = Subcommand
-  { name = "spec:deploy"
-  , description = "Deploy changes from an App's Stack specification"
+  { name = "deploy"
+  , description = "Deploy specifications"
   , parse = parseSpecDeployOptions
   , run = runSpecDeploy
   }
@@ -58,7 +58,7 @@ deploy = Subcommand
 version :: HasLogFunc env => Subcommand () env
 version = Subcommand
   { name = "version"
-  , description = "Output the Platform CLI's current version"
+  , description = "Output the version"
   , parse = pure ()
   , run = \() -> logVersion
   }
