797633c714f4189685191969d1bd8f86399832f7
patrick brisbin <pbrisbin@gmail.com>
2022-06-16 13:21:55 -0400

    Polish up version sub-command

--- src/Stackctl/Version.hs
+++ src/Stackctl/Version.hs
@@ -6,6 +6,7 @@ import Stackctl.Prelude
 
 import Data.Version
 import qualified Paths_stackctl as Pkg
+import Prelude (putStrLn)
 
-logVersion :: MonadLogger m => m ()
-logVersion = logInfo $ fromString $ showVersion Pkg.version
+logVersion :: MonadIO m => m ()
+logVersion = liftIO $ putStrLn $ ("Stackctl v" <>) $ showVersion Pkg.version
