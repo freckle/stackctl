951fba9b1ac0e3dd62f8b03db8279006bcbdbe98
patrick brisbin <pbrisbin@gmail.com>
2022-06-17 10:59:17 -0400

    Generalize runSubcommand for varying options types

--- src/Stackctl/Subcommand.hs
+++ src/Stackctl/Subcommand.hs
@@ -2,13 +2,16 @@ module Stackctl.Subcommand
   ( Subcommand(..)
   , subcommand
   , runSubcommand
+  , runSubcommand'
   ) where
 
 import Stackctl.Prelude
 
 import Options.Applicative
 import qualified Stackctl.CLI as CLI
+import Stackctl.Colors
 import Stackctl.Options
+import Stackctl.VerboseOption
 
 data Subcommand options env = Subcommand
   { name :: Text
@@ -22,13 +25,16 @@ subcommand Subcommand {..} =
   command (unpack name) (run <$> withInfo description parse)
 
 runSubcommand :: Mod CommandFields (CLI.AppT (CLI.App Options) IO ()) -> IO ()
-runSubcommand x = do
-  (options, act) <-
-    execParser
-    $ withInfo "Work with Stack specifications"
-    $ (,)
-    <$> optionsParser
-    <*> subparser x
+runSubcommand = runSubcommand' "Work with Stack specifications" optionsParser
+
+runSubcommand'
+  :: (HasVerboseOption options, HasColorOption options)
+  => Text
+  -> Parser options
+  -> Mod CommandFields (CLI.AppT (CLI.App options) IO ())
+  -> IO ()
+runSubcommand' x op sp = do
+  (options, act) <- execParser $ withInfo x $ (,) <$> op <*> subparser sp
   CLI.runAppT options act
 
 withInfo :: Text -> Parser a -> ParserInfo a
