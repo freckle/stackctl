99b3c631198c14902b16d29e76de61a41945fa20
patrick brisbin <pbrisbin@gmail.com>
2023-01-11 08:07:18 -0500

    Refactor RequiredVersion, add instances
    https://app.asana.com/0/13211253278157/1203652238195585/f
    
    We need `Eq` to use `RequiredVersion` in tests. And we need `ToJSON` for
    structured logging.
    
    The current `Show` instance was custom and doesn't render a valid
    Haskell syntax, which is an anti-pattern. The reason for this is that
    the `requiredVersionCompare` member was itself a function, so we
    couldn't just derive a `stock` `Show`. By removing that member, and
    instead holding a new `RequiredVersionOp` enumeration that can be
    converted to it, we can now derive `stock` `Eq` and `Show`. The new
    `requiredVersionOpToText` and `requiredVersionOpCompare` functions will
    need to be kept in sync, but I don't see them changing very frequently
    (and we have great tests here).
    
    We retain the more human-readable format in the new `ToJSON` instance,
    because we need it in `FromJSON` (to parse Yaml correctly) and it must
    of course round-trip with `ToJSON`.

--- src/Stackctl/Config/RequiredVersion.hs
+++ src/Stackctl/Config/RequiredVersion.hs
@@ -1,5 +1,6 @@
 module Stackctl.Config.RequiredVersion
   ( RequiredVersion(..)
+  , requiredVersionToText
   , requiredVersionFromText
   , isRequiredVersionSatisfied
 
@@ -18,18 +19,23 @@ import qualified Data.Version as Version
 import Text.ParserCombinators.ReadP (readP_to_S)
 
 data RequiredVersion = RequiredVersion
-  { requiredVersionOp :: Text
-  , requiredVersionCompare :: Version -> Version -> Bool
+  { requiredVersionOp :: RequiredVersionOp
   , requiredVersionCompareWith :: Version
   }
-
-instance Show RequiredVersion where
-  show RequiredVersion {..} =
-    unpack requiredVersionOp <> " " <> showVersion requiredVersionCompareWith
+  deriving stock (Eq, Show)
 
 instance FromJSON RequiredVersion where
   parseJSON =
     withText "RequiredVersion" $ either fail pure . requiredVersionFromText
+
+instance ToJSON RequiredVersion where
+  toJSON = toJSON . requiredVersionToText
+  toEncoding = toEncoding . requiredVersionToText
+
+requiredVersionToText :: RequiredVersion -> Text
+requiredVersionToText RequiredVersion {..} =
+  requiredVersionOpToText requiredVersionOp <> " " <> pack
+    (showVersion requiredVersionCompareWith)
 
 requiredVersionFromText :: Text -> Either String RequiredVersion
 requiredVersionFromText = fromWords . T.words
@@ -44,21 +50,21 @@ requiredVersionFromText = fromWords . T.words
         <> " did not parse as optional operator and version string"
 
   parseRequiredVersion :: Text -> Text -> Either String RequiredVersion
-  parseRequiredVersion op w = do
-    v <- parseVersion w
+  parseRequiredVersion op w = RequiredVersion <$> parseOp op <*> parseVersion w
 
-    case op of
-      "=" -> Right $ RequiredVersion op (==) v
-      "<" -> Right $ RequiredVersion op (<) v
-      "<=" -> Right $ RequiredVersion op (<=) v
-      ">" -> Right $ RequiredVersion op (>) v
-      ">=" -> Right $ RequiredVersion op (>=) v
-      "=~" -> Right $ RequiredVersion op (=~) v
-      _ ->
-        Left
-          $ "Invalid comparison operator ("
-          <> unpack op
-          <> "), may only be =, <, <=, >, >=, or =~"
+  parseOp :: Text -> Either String RequiredVersionOp
+  parseOp op = case op of
+    "=" -> Right RequiredVersionEQ
+    "<" -> Right RequiredVersionLT
+    "<=" -> Right RequiredVersionLTE
+    ">" -> Right RequiredVersionGT
+    ">=" -> Right RequiredVersionGTE
+    "=~" -> Right RequiredVersionIsh
+    _ ->
+      Left
+        $ "Invalid comparison operator ("
+        <> unpack op
+        <> "), may only be =, <, <=, >, >=, or =~"
 
   parseVersion :: Text -> Either String Version
   parseVersion t =
@@ -68,6 +74,38 @@ requiredVersionFromText = fromWords . T.words
       $ readP_to_S Version.parseVersion s
     where s = unpack t
 
+isRequiredVersionSatisfied :: RequiredVersion -> Version -> Bool
+isRequiredVersionSatisfied RequiredVersion {..} =
+  (`requiredVersionCompare` requiredVersionCompareWith)
+  where requiredVersionCompare = requiredVersionOpCompare requiredVersionOp
+
+data RequiredVersionOp
+  = RequiredVersionEQ
+  | RequiredVersionLT
+  | RequiredVersionLTE
+  | RequiredVersionGT
+  | RequiredVersionGTE
+  | RequiredVersionIsh
+  deriving stock (Eq, Show)
+
+requiredVersionOpToText :: RequiredVersionOp -> Text
+requiredVersionOpToText = \case
+  RequiredVersionEQ -> "=="
+  RequiredVersionLT -> "<"
+  RequiredVersionLTE -> "<="
+  RequiredVersionGT -> ">"
+  RequiredVersionGTE -> ">="
+  RequiredVersionIsh -> "=~"
+
+requiredVersionOpCompare :: RequiredVersionOp -> Version -> Version -> Bool
+requiredVersionOpCompare = \case
+  RequiredVersionEQ -> (==)
+  RequiredVersionLT -> (<)
+  RequiredVersionLTE -> (<=)
+  RequiredVersionGT -> (>)
+  RequiredVersionGTE -> (>=)
+  RequiredVersionIsh -> (=~)
+
 (=~) :: Version -> Version -> Bool
 a =~ b = a >= b && a < incrementVersion b
  where
@@ -75,7 +113,3 @@ a =~ b = a >= b && a < incrementVersion b
   onVersion f = makeVersion . f . versionBranch
   backwards f = reverse . f . reverse
   onHead f as = maybe as (uncurry (:) . first f) $ uncons as
-
-isRequiredVersionSatisfied :: RequiredVersion -> Version -> Bool
-isRequiredVersionSatisfied RequiredVersion {..} =
-  (`requiredVersionCompare` requiredVersionCompareWith)
