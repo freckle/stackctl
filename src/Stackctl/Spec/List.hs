aa2b96533a51be799a20665c15027837cf493215
patrick brisbin <pbrisbin@gmail.com>
2023-08-30 09:05:51 -0400

    Improve status indicators in stackctl-ls
    - Add more granularity
    
      A Stack that is not yet deployed at all may look deployed because it
      exists once the first change set is in review. Having a separate
      indication for this is valuable -- we might as well make more too.
    
    - Print a legend at the end
    
      Now that we have more complicated indicators, showing what they mean
      seems valuable. A `--no-legend` option was added to suppress this, in
      case the output is being parsed line-wise.

--- src/Stackctl/Spec/List.hs
+++ src/Stackctl/Spec/List.hs
@@ -7,6 +7,7 @@ module Stackctl.Spec.List
 import Stackctl.Prelude
 
 import Blammo.Logging.Logger (pushLoggerLn)
+import qualified Data.Text as T
 import Options.Applicative
 import Stackctl.AWS
 import Stackctl.AWS.Scope
@@ -17,12 +18,21 @@ import Stackctl.FilterOption (HasFilterOption)
 import Stackctl.Spec.Discover
 import Stackctl.StackSpec
 
-data ListOptions = ListOptions
-
--- brittany-disable-next-binding
+newtype ListOptions = ListOptions
+  { loLegend :: Bool
+  }
 
 parseListOptions :: Parser ListOptions
-parseListOptions = pure ListOptions
+parseListOptions =
+  ListOptions
+    <$> ( not
+            <$> switch
+              ( mconcat
+                  [ long "no-legend"
+                  , help "Don't print indicators legend at the end"
+                  ]
+              )
+        )
 
 runList
   :: ( MonadUnliftIO m
@@ -39,23 +49,76 @@ runList
      )
   => ListOptions
   -> m ()
-runList _ = do
-  Colors {..} <- getColorsLogger
+runList ListOptions {..} = do
+  colors@Colors {..} <- getColorsLogger
 
   forEachSpec_ $ \spec -> do
     let
       path = stackSpecFilePath spec
       name = stackSpecStackName spec
 
-    exists <- isJust <$> awsCloudFormationDescribeStackMaybe name
+    mStackStatus <-
+      fmap (^. stack_stackStatus)
+        <$> awsCloudFormationDescribeStackMaybe name
+
+    -- logInfo $ "" :# ["stackStatus" .= mStackStatus]
 
     let
+      indicator = maybe NotDeployed statusIndicator mStackStatus
+
       formatted :: Text
       formatted =
         "  "
-          <> (if exists then green "✓ " else yellow "✗ ")
+          <> indicatorIcon colors indicator
+          <> " "
           <> cyan (unStackName name)
           <> " => "
           <> magenta (pack path)
 
     pushLoggerLn formatted
+
+  let legendItem i = indicatorIcon colors i <> " " <> indicatorDescription i
+
+  when loLegend
+    $ pushLoggerLn
+    $ "\nLegend:\n  "
+    <> T.intercalate ", " (map legendItem [minBound .. maxBound])
+
+data Indicator
+  = Deployed
+  | DeployFailed
+  | NotDeployed
+  | Reviewing
+  | Deploying
+  | Unknown
+  deriving stock (Bounded, Enum)
+
+indicatorIcon :: Colors -> Indicator -> Text
+indicatorIcon Colors {..} = \case
+  Deployed -> green "✓"
+  DeployFailed -> red "✗"
+  NotDeployed -> yellow "_"
+  Reviewing -> yellow "∇"
+  Deploying -> cyan "⋅"
+  Unknown -> magenta "?"
+
+indicatorDescription :: Indicator -> Text
+indicatorDescription = \case
+  Deployed -> "deployed"
+  DeployFailed -> "failed or rolled back"
+  NotDeployed -> "doesn't exist"
+  Reviewing -> "reviewing"
+  Deploying -> "deploying"
+  Unknown -> "unknown"
+
+statusIndicator :: StackStatus -> Indicator
+statusIndicator = \case
+  StackStatus_REVIEW_IN_PROGRESS -> Reviewing
+  StackStatus_ROLLBACK_COMPLETE -> DeployFailed
+  x | statusSuffixed "_IN_PROGRESS" x -> Deploying
+  x | statusSuffixed "_FAILED" x -> DeployFailed
+  x | statusSuffixed "_ROLLBACK_COMPLETE" x -> DeployFailed
+  x | statusSuffixed "_COMPLETE" x -> Deployed
+  _ -> Unknown
+ where
+  statusSuffixed x = (x `T.isSuffixOf`) . fromStackStatus
