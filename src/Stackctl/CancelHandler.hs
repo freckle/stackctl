39a5ea160f6d89407766dd0faacccdd7cc392499
patrick brisbin <pbrisbin@gmail.com>
2024-09-11 09:14:45 -0400

    Cancel UpdateStack operations on Ctl-C
    ![](https://files.pbrisbin.com/screenshots/screenshot.3323976.png)

---
+++ src/Stackctl/CancelHandler.hs
@@ -0,0 +1,33 @@
+module Stackctl.CancelHandler
+  ( with
+  , install
+  , remove
+  , trigger
+  ) where
+
+import Stackctl.Prelude
+
+import System.Posix.Signals
+
+-- | Install a 'keyboardSignal handler, run an action, then remove it
+with :: MonadUnliftIO m => m a -> m b -> m b
+with f = bracket (install f) (const remove) . const
+
+-- | Install a 'keyboardSignal handler that runs the given action once
+install :: MonadUnliftIO m => m a -> m ()
+install f = do
+  withRunInIO $ \runInIO -> do
+    let handler = Catch $ void $ do
+          remove -- so next Ctl-C will truly cancel
+          runInIO f
+    void $ installHandler keyboardSignal handler Nothing
+
+-- | Remove the current handler for 'keyboardSignal (i.e. install 'Default')
+remove :: MonadIO m => m ()
+remove = liftIO $ void $ installHandler keyboardSignal Default Nothing
+
+-- | Trigger the installed 'keyboardSignal handler
+--
+-- This is used by our test suite.
+trigger :: MonadIO m => m ()
+trigger = liftIO $ raiseSignal keyboardSignal
