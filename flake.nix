cb12d8ce40e2c36142a96a1f42cf4abee4e8b9ef
Chris Martin <ch.martin@gmail.com>
2025-10-13 13:10:29 -0600

    update nix flake (#102)

--- flake.nix
+++ flake.nix
@@ -1,15 +1,12 @@
 {
   inputs = {
-    stable.url = "github:nixos/nixpkgs/nixos-23.11";
-    freckle.url = "git+ssh://git@github.com/freckle/flakes?dir=main";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05";
+    freckle.url = "github:freckle/flakes?dir=main";
     flake-utils.url = "github:numtide/flake-utils";
   };
   outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
     let
-      nixpkgsArgs = { inherit system; config = { }; };
-      nixpkgs = {
-        stable = import inputs.stable nixpkgsArgs;
-      };
+      nixpkgs = inputs.nixpkgs.legacyPackages.${system};
       freckle = inputs.freckle.packages.${system};
       freckleLib = inputs.freckle.lib.${system};
     in
@@ -17,31 +14,31 @@
       packages = {
         awscli = freckle.aws-cli-2-11-x;
 
-        cabal = nixpkgs.stable.cabal-install;
+        cabal = nixpkgs.cabal-install;
 
         fourmolu = freckle.fourmolu-0-13-x;
 
         ghc = freckleLib.haskellBundle {
-          ghcVersion = "ghc-9-6-3";
+          ghcVersion = "ghc-9-8-4";
           packageSelection = p: [ ];
           enableHLS = true;
         };
 
         hlint =
-          nixpkgs.stable.haskell.lib.justStaticExecutables
-            nixpkgs.stable.hlint;
+          nixpkgs.haskell.lib.justStaticExecutables
+            nixpkgs.hlint;
 
-        stack = nixpkgs.stable.writeShellApplication {
+        stack = nixpkgs.writeShellApplication {
           name = "stack";
           text = ''
-            ${nixpkgs.stable.stack}/bin/stack --system-ghc --no-nix "$@"
+            ${nixpkgs.stack}/bin/stack --system-ghc --no-nix "$@"
           '';
         }
         ;
       };
 
-      devShells.default = nixpkgs.stable.mkShell {
-        buildInputs = with (nixpkgs.stable); [
+      devShells.default = nixpkgs.mkShell {
+        buildInputs = with (nixpkgs); [
           pcre
           pcre.dev
           zlib
@@ -62,4 +59,15 @@
         '';
       };
     });
+
+  nixConfig = {
+    extra-substituters = [
+      "https://freckle.cachix.org"
+      "https://freckle-private.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "freckle.cachix.org-1:WnI1pZdwLf2vnP9Fx7OGbVSREqqi4HM2OhNjYmZ7odo="
+      "freckle-private.cachix.org-1:zbTfpeeq5YBCPOjheu0gLyVPVeM6K2dc1e8ei8fE0AI="
+    ];
+  };
 }
